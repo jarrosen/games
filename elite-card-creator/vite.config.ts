2ff4c5a2887246c5f1679b29a4f4316d7ecfbf6c
dannyzen <danny.rosen@gmail.com>
2026-02-08 17:22:23 -0500

    fix(elite-card-creator): revert base path to /games/elite-card-creator/

--- elite-card-creator/vite.config.ts
+++ elite-card-creator/vite.config.ts
@@ -19,6 +19,6 @@ export default defineConfig(({ mode }) => {
         '@': path.resolve(__dirname, '.'),
       }
     },
-    base: '/elite-card-creator/',
+    base: '/games/elite-card-creator/',
   };
 });
